64eb5a987d23d16b5ab52ecf638bdd0233aa0214
ren.meta@gmail.com <ren.meta@gmail.com>
2020-08-15 20:34:49 -0400

    extending Point class

--- classes/classes.scala
+++ classes/classes.scala
@@ -1,6 +1,7 @@
 import java.io._
 
-class Point(xc: Int, yc: Int) {
+// Basic Class (when extended - Superclass)
+class Point(val xc: Int, val yc: Int) {
     var x: Int = xc
     var y: Int = yc
 
@@ -14,6 +15,8 @@ class Point(xc: Int, yc: Int) {
 
 object ClassDemo {
     def main(args: Array[String]) {
+        // Create objects using a keyword "new" and then 
+        // you can access class fields and methods
         val pt = new Point(10, 20);
 
         // Move to a new location
@@ -21,9 +24,43 @@ object ClassDemo {
     }
 }
 
-//$ scalac classes.scala
-//$ scala ClassDemo
-
+// $ scalac classes.scala
+// $ scala ClassDemo
 // Outputs:
 // Point x location: 20
 // Point y location: 30
+
+// Extends (inheritance)
+// Extends inherits (non-private) from Point class
+// and makes Location a subtype of Point class (subclass)
+class Location(override val xc: Int, override val yc: Int,
+        val zc: Int) extends Point(xc, yc) {
+        var z: Int = zc
+        // No override due to different definitions
+        // (former takes 2 arguments, this takes 3 args)
+        def move(dx: Int, dy: Int, dz: Int) {
+            x = x + dx
+            y = y + dy
+            z = z + dz
+            println("Point x location: " + x);
+            println("Point y location: " + y);
+            println("Point z location: " + z);
+        }
+    }
+object ExtendDemo {
+    def main(args: Array[String]) {
+        val loc = new Location(10, 20, 35);
+
+        // Move to a new location
+        loc.move(10, 20, 5);
+    }
+}
+
+// \>scalac Demo.scala
+// \>scala Demo
+// Output:
+// Point x location : 20
+// Point y location : 30
+// Point z location : 20
+
+
